5363f5c00c4214de979814352ec63b857c827af5
lms7127 <lms7127@naver.com>
2023-07-28 15:19:09 +0900

    add requirements.txt

--- inpimg/models.py
+++ inpimg/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 # Create your models here.
-
+import uuid
+import os
 
 class Profile(models.Model):
     id= models.IntegerField(primary_key=True)
-    image = models.ImageField(null=True, upload_to='images/')
+    
+    def upload_to_func(instance, filename):
+        file_name=uuid.uuid4().hex
+        extension=os.path.splitext(filename)[-1].lower()
+        return "/".join(
+            [file_name, extension,]
+        )
+    
+    image = models.ImageField(null=True, upload_to=upload_to_func)
     country=models.CharField(null=True,  max_length=50)
     def __str__(self):
         return self.title
--- inpimg/views.py
+++ inpimg/views.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*- 
 from django.shortcuts import render,redirect
 from django.views.decorators.csrf import csrf_exempt
 from .models import Profile
+import numpy as np
 import os
 import cv2
 import easyocr
 import logging
+import urllib
+#-*- coding: utf-8 -*-
+
 
 @csrf_exempt
 def index(request):
@@ -30,6 +35,7 @@ def upload_create(request):
         form.id=tar
     except: #이미지가 없어도 그냥 지나가도록-!
         pass
+    
     form.save()
     
     
@@ -43,27 +49,36 @@ def profile(request):
     
     
     profile=Profile.objects.get(id=tar) 
-    if last_image!='':
-        os.remove(last_image.path)
-    last_image=profile.image
-    
+
     image = profile.image.url
+    
     con=profile.country
     
     currentPath = os.getcwd()
     results=[]
     reader = easyocr.Reader([con], gpu=True)
     
-    THRESHOLD = 0.01
+    THRESHOLD = 0.1
+    
+    try:
+        last=Profile.objects.get(id=tar-1)
+        last_image=last.image.url
+        os.remove(currentPath+last_image)
+    except:
+        pass
     
     
-    result = reader.readtext(currentPath+image)
     
-    for bbox, text, conf in result:
-        if conf > THRESHOLD:
-            results.append(text)
+    
+    try:
+        result = reader.readtext(currentPath+image)
+        for bbox, text, conf in result:
+            if conf > THRESHOLD:
+                results.append(text)
+    except:    # 예외가 발생했을 때 실행됨
+        results=['이미지를 다시 입력해주세요.']
+        profile=None
     
         
-    return render(request,'inpimg/home.html',{'profile':profile, 'results':results})
-
+    return render(request,'inpimg/result.html',{'profile':profile, 'results':results})
 
